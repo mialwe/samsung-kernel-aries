0fe3383160f6d7186ebb64a8cd4f8ed870efddf2
Michael Weingärtner <mialwe@googlemail.com>
2012-04-10 22:49:53 +0200

    remove IO scheduler setting

--- initramfs/ics_init/sbin/boot-patch.sh
+++ initramfs/ics_init/sbin/boot-patch.sh
@@ -221,7 +221,6 @@ MMC=`$BB ls -d /sys/block/mmc*`
 
 # general IO tweaks
 for i in $MTD $MMC $LOOP;do
-    echo "$iosched" > $i/queue/scheduler
     echo 0 > $i/queue/rotational
     echo 0 > $i/queue/iostats
 done
