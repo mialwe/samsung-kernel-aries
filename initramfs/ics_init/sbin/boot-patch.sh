6b6ca9825863f4dad25bd8192b445d5b376c019a
Michael Weingärtner <mialwe@googlemail.com>
2012-04-10 22:25:38 +0200

    set default sdcard read_ahead to 256Kb, remove unneeded init.d stuff

--- initramfs/ics_init/sbin/boot-patch.sh
+++ initramfs/ics_init/sbin/boot-patch.sh
@@ -219,7 +219,7 @@ cat_msg_sysfile "default: " /sys/devices/virtual/bdi/default/read_ahead_kb
 if $BB [[ "$readahead" -eq 64 || "$readahead" -eq 128 || "$readahead" -eq 256 || "$readahead" -eq 512  || "$readahead" -eq 1024 || "$readahead" -eq 2048 || "$readahead" -eq 3096 ]];then
     echo "CPU: found vaild readahead: <$readahead>"
 else
-    readahead=512
+    readahead=256
 fi
 
 echo $readahead > /sys/devices/virtual/bdi/179:0/read_ahead_kb
@@ -306,31 +306,9 @@ if $BB [ -f /data/local/$CONFFILE ];then
     echo "configfile /data/local/midnight_options.conf found, checking values..."
     if $BB [ "`/system/xbin/busybox grep INITD /data/local/$CONFFILE`" ]; then
         echo "starting init.d script execution..."
-        echo $(date) USER EARLY INIT START from /system/etc/init.d
-        if cd /system/etc/init.d >/dev/null 2>&1 ; then
-            for file in E* ; do
-                if ! cat "$file" >/dev/null 2>&1 ; then continue ; fi
-                echo "init.d: START '$file'"
-                /system/bin/sh "$file"
-                echo "init.d: EXIT '$file' ($?)"
-            done
-        fi
-        echo $(date) USER EARLY INIT DONE from /system/etc/init.d
-
         echo $(date) USER INIT START from /system/etc/init.d
         if cd /system/etc/init.d >/dev/null 2>&1 ; then
             for file in S* ; do
-                if ! ls "$file" >/dev/null 2>&1 ; then continue ; fi
-                echo "init.d: START '$file'"
-                /system/bin/sh "$file"
-                echo "init.d: EXIT '$file' ($?)"
-            done
-        fi
-        echo $(date) USER INIT DONE from /system/etc/init.d
-
-        echo $(date) USER INIT START from /system/etc/init.d
-        if cd /system/etc/init.d >/dev/null 2>&1 ; then
-            for file in [0-9][0-9]* ; do
                 if ! ls "$file" >/dev/null 2>&1 ; then continue ; fi
                 echo "init.d: START '$file'"
                 /system/bin/sh "$file"
